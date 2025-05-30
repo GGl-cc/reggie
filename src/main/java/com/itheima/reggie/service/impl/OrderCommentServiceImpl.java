98f55e52d4dd21e0e0ff8c7e91a30de451d06078
GGI-cc <chen24014913@qq.com>
2025-05-30 17:13:47 +0800

    管理加了查看评论但连不上

--- src/main/java/com/itheima/reggie/service/impl/OrderCommentServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/OrderCommentServiceImpl.java
@@ -6,11 +6,17 @@ import com.itheima.reggie.service.OrderCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Service
 public class OrderCommentServiceImpl implements OrderCommentService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderCommentServiceImpl.class);
     @Autowired
     private OrderCommentMapper orderCommentMapper;
 
@@ -26,7 +32,18 @@ public class OrderCommentServiceImpl implements OrderCommentService {
 
     @Override
     public List<OrderComment> getAllComments() {
-        return orderCommentMapper.selectAll();
+        log.info("返回模拟评论数据");
+        List<OrderComment> mockComments = new ArrayList<>();
+        OrderComment comment = new OrderComment();
+        comment.setId(1L);
+        comment.setOrderId(1L);
+        comment.setUserId(1L);
+
+        comment.setContentText("这是一条测试评论");
+        comment.setCreateTime(LocalDateTime.now());
+        mockComments.add(comment);
+        return mockComments;
+       // return orderCommentMapper.selectAll();
     }
 
     @Override
